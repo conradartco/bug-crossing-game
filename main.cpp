ca4c82f6547b4633cfff5d1e590c544baeb9cc0c
Jonathan Conrad <jc@Jonathans-MBP.home>
2022-09-15 14:30:52 -0700

    feat: Added main character along with run animation attributes

--- main.cpp
+++ main.cpp
@@ -15,16 +15,26 @@ int main()
     const float speed{4.0f};
 
     // load character
+    Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
+    Texture2D knight_idle = LoadTexture("characters/knight_idle_spritesheet.png");
+    Texture2D knight_run = LoadTexture("characters/knight_run_spritesheet.png");
 
-    // character position on screen
+    // character position at the center of screen
+    Vector2 knightPos{
+        windowWidth/2.0f - mapScale * (0.5f * (float)knight.width/6.0f),
+        windowHeight/2.0f - mapScale * (0.5f * (float)knight.height)
+    };
 
-    // character facing
+    // character facing / 1 : facing right / -1 : facing left
+    float rightLeft{1.f};
 
     // animation variables
-
+    float runningTime{};
     // current animation frame
-
+    int frame{};
+    const int maxFrames{6};
     // animation frame update
+    const float updateTime{1.f/12.f};
 
     // call character
 
@@ -52,15 +62,37 @@ int main()
         // Vector2Length returns a float so we can determine the length of movement direction // if zero, no movement
         if (Vector2Length(direction) != 0.0)
         {
+            knight = knight_run;
             // set mapPos = mapPos - direction
-            
+            // scale the movement using speed and the normalized direction
             mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
+            // check and change facing direction
+            direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
+        }
+        else
+        {
+            knight = knight_idle;
         }
 
         // draw the map
         DrawTextureEx(map, mapPos, 0.0, mapScale, WHITE);
 
         // update animation frame
+        runningTime += GetFrameTime();
+        // increase frame count per second
+        if (runningTime >= updateTime)
+        {
+            frame++;
+            runningTime = 0.f;
+            if (frame > maxFrames) frame = 0;
+        }
+
+        // initialize the location on the spritesheet and multiply rightLeft to scale, flipping the value and changing direction
+        Rectangle source{frame * (float)knight.width/6.f, 0.f, rightLeft * (float)knight.width/6.f, (float)knight.height};
+        // the location in the window to draw the character
+        Rectangle dest{knightPos.x, knightPos.y, mapScale * (float)knight.width/6.0f, mapScale * (float)knight.height};
+        // draw the character
+        DrawTexturePro(knight, source, dest, Vector2{}, 0.f, WHITE);
 
         // draw the props
 
